c2c14891efe28e5f1c123f3287ce48372daf6ebe
Disturbo <nottodisturb.disturbo@gmail.com>
2023-01-09 02:42:02 +0100

    Complete archive up to 6.00 PBE

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import shutil
 import numpy as np
 import pandas as pd
 
-from versionutils import get_game_version
+from versionutils import get_game_version, get_processed_versions
 
 SHIPPING_EXE = "VALORANT-Win64-Shipping.exe"
 
@@ -15,6 +15,17 @@ MANIFESTS_PATH = ".\\out\\manifests.json"
 
 TEMP_PATH = ".\\temp\\"
 SHIPPING_PATH = TEMP_PATH + "ShooterGame\\Binaries\\Win64\\" + SHIPPING_EXE
+SHIPPING_PBE_PATH = TEMP_PATH + "ShooterGame\\Binaries\\Win64\\VALORANT-Win64-Shipping_PBE.exe"
+
+
+def load_manifests():
+    with open(MANIFESTS_PATH, mode="r", encoding="utf-8") as manifests_file:
+        return json.load(manifests_file)
+
+
+def save_manifests(manifests):
+    with open(MANIFESTS_PATH, mode="w", encoding="utf-8") as manifests_file:
+        json.dump(manifests, manifests_file, indent=4)
 
 
 def create_temp_folder():
@@ -33,13 +44,6 @@ def extract_manifest_id(manifest_url: str):
     return manifest_url.split(".manifest")[0].split("/")[-1]
 
 
-def extract_version_number(client_version: str):
-    if client_version.startswith("pbe"):
-        return ""
-    split_version = client_version.split("-")
-    return split_version[1] + ".00." + split_version[-1]
-
-
 # Riot Archive Project
 # https://docs.google.com/spreadsheets/d/18Fl88fB2sI57OFhOFSHtcOlHZG9kMS0uU3kjFxzv_EA/edit#gid=181398849
 def get_valorant_rap():
@@ -51,11 +55,11 @@ def get_valorant_rap():
     return rap_valorant[["Manifest", "Timestamp"]]
 
 
-def is_manifest_in_archive(manifests: dict, manifest: str):
+def get_manifest_data(manifests: list, manifest: str):
     for archived_manifest in manifests:
         if archived_manifest["manifest"] == manifest:
-            return True
-    return False
+            return archived_manifest
+    return None
 
 
 def fetch_shipping_exe(manifest: str):
@@ -63,38 +67,52 @@ def fetch_shipping_exe(manifest: str):
         else f"https://valorant.secure.dyn.riotcdn.net/channels/public/releases/{manifest}.manifest"
     bundle_url = "https://valorant.secure.dyn.riotcdn.net/channels/public/bundles"
     command = f"{MD_PATH} {manifest_url} -b {bundle_url} --filter {SHIPPING_EXE} -o {TEMP_PATH}"
-
-    create_temp_folder()
     os.system(command)
 
 
-def archive_valorant_rap(manifests, rap_valorant: pd.DataFrame):
+def archive_wob(manifests, wob):
+    for version in wob:
+
+        if get_manifest_data(manifests, version["manifest"]):
+            print(f"{version['manifest']} (WOB): existing archive")
+            continue
+
+        manifests.append(version)
+        print(f"{version['manifest']} (WOB): new archive")
+    # WOB is quick, save only after all al done
+    save_manifests(manifests)
+
+
+def archive_valorant_rap(manifests: list, rap_valorant: pd.DataFrame):
     for _, row in rap_valorant.iterrows():
         manifest = extract_manifest_id(row["Manifest"])
-
-        if not is_manifest_in_archive(manifests, manifest):
-            print(f"Archiving {manifest}...")
-            fetch_shipping_exe(row["Manifest"])
-            client_version = get_game_version(SHIPPING_PATH)
-            archive_manifest = {
-                "manifest": manifest,
-                "version_number": extract_version_number(client_version),
-                "client_version": client_version,
-                "release_timestamp": row["Timestamp"]
-            }
-            manifests.append(archive_manifest)
+        manifest_data = get_manifest_data(manifests, manifest)
+        if manifest_data:
+            print(f"{manifest} (Riot Archive): existing archive")
+            continue
+        print(f"{manifest} (Riot Archive): new archive")
+        create_temp_folder()
+        fetch_shipping_exe(row["Manifest"])
+        client_version = get_game_version(SHIPPING_PATH)
+        delete_temp_folder()
+        print()
+        archive_manifest = {
+            "manifest": manifest,
+            "branch": client_version["branch"],
+            "version": client_version["version"],
+            "release_timestamp": row["Timestamp"]
+        }
+        manifests.append(archive_manifest)
+        # Riot Archive requires downloads, save after every manifest
+        save_manifests(manifests)
 
 
 def main():
-    with open(MANIFESTS_PATH, mode="r", encoding="utf-8") as manifests_file:
-        manifests = json.load(manifests_file)
-
+    manifests = load_manifests()
+    wob = get_processed_versions()
     rap_valorant = get_valorant_rap()
+    archive_wob(manifests, wob)
     archive_valorant_rap(manifests, rap_valorant)
-    delete_temp_folder()
-
-    with open(MANIFESTS_PATH, mode="w", encoding="utf-8") as manifests_file:
-        json.dump(manifests, manifests_file, indent=4)
 
 
 if __name__ == "__main__":
